13e4814fed889af2154b068873cb1302fd3b858d
llsmaker <otaldoleles@hotmail.com>
2026-02-06 18:09:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,8 @@
-const CACHE_NAME = 'dcl-v1';
+const cacheName = 'dcl-v1';
 const assets = ['./', 'index.html', 'manifest.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
