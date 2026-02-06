cf6fdde402cc592e7b7079638b685e90ec90d532
llsmaker <otaldoleles@hotmail.com>
2026-02-06 20:01:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,33 @@
-const cacheName = 'dcl-v1';
-const assets = ['./', 'index.html', 'manifest.json'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+const CACHE_NAME = 'dcl-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
