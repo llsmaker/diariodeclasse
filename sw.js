5f8d9008efd4c8f2648b244df4bf84e1880600f6
llsmaker <otaldoleles@hotmail.com>
2026-02-07 14:39:47 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-const cacheName = 'dcl-v1'; // Mude o v1 se quiser forçar atualização depois
-
+const cacheName = 'dcl - v2';
 self.addEventListener('install', e => {
-  self.skipWaiting(); // Força a instalação imediata no PC
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json'
-    ]))
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(['./', './index.html', './manifest.json'])));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim()); // Assume o controle do site na hora
-});
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
