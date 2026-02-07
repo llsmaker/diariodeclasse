55e0858dd284140e8c290e710c673e709b700321
llsmaker <otaldoleles@hotmail.com>
2026-02-07 13:51:38 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
 
-// Essencial para o Chrome não dar erro de instalação
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+// Este evento 'fetch' é o que faz o Chrome liberar o botão de instalar no PC
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => new Response("offline"))
+    fetch(event.request).catch(() => new Response("Offline"))
   );
 });
